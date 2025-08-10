e403190073a75b2ffbad32f767b1556995bd709c
mfstratton <michaelfstratton@gmail.com>
2025-08-09 23:08:05 -0500

    Prep iOS build v29

--- app/index.tsx
+++ app/index.tsx
@@ -48,7 +48,7 @@ interface MapboxSuggestion {
     metadata?: any;
 }
 
-const GENRE_OPTIONS = ["Alternative", "Blues", "Children's Music", "Classical", "Country", "Dance/Electronic", "Folk", "Hip-Hop/Rap", "Jazz", "Latin", "Metal", "New Age", "Pop", "R&B", "Reggae", "Religious", "Rock", "World"];
+const GENRE_OPTIONS = ["Alternative", "Ballads/Romantic", "Blues", "Children's Music", "Classical", "Country", "Dance/Electronic", "Folk", "Hip-Hop/Rap", "Holiday", "Jazz", "Latin", "Metal", "New Age", "Pop", "R&B", "Reggae", "Religious", "Rock", "World"];
 const RADIUS_OPTIONS = [5, 10, 20, 30, 40, 60];
 
 export default function SearchInputScreen() {
